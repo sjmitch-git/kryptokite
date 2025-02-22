1b4cab3d6d32cc1b4d6a981324928f256e7ac7c5
sjmitch-git <sjmitch20@outlook.com>
2025-02-22 14:48:21 +0000

    Adds Suspens to MyCoins

--- src/app/mycoins/page.tsx
+++ src/app/mycoins/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import Hero from "@/components/Hero";
 import MyCoinsList from "@/components/coins/MyCoinsList";
 
@@ -5,7 +7,9 @@ const MyCoins = () => {
   return (
     <article>
       <Hero title="My Coins" description="Page description" />
-      <MyCoinsList />
+      <Suspense>
+        <MyCoinsList />
+      </Suspense>
     </article>
   );
 };
