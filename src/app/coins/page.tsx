88de4633926f20eaea2ee78880a41c8203ee4025
sjmitch-git <sjmitch20@outlook.com>
2025-02-22 14:52:31 +0000

    Adds Suspense to Coins

--- src/app/coins/page.tsx
+++ src/app/coins/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import Hero from "@/components/Hero";
 import CoinsList from "@/components/coins/CoinsList";
 
@@ -5,7 +7,9 @@ export default function Coins() {
   return (
     <article>
       <Hero title="Coins" description="Page description" />
-      <CoinsList />
+      <Suspense>
+        <CoinsList />
+      </Suspense>
     </article>
   );
 }
