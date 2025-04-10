1c674df3333d3d0358fa01c6a60eff9904cc4646
sjmitch-git <sjmitch20@outlook.com>
2025-04-10 10:00:17 +0100

    Adds news section

--- src/app/api/news/generate/route.ts
+++ src/app/api/news/generate/route.ts
@@ -8,11 +8,11 @@ const openai = new OpenAI({
 
 const fetchCoinGeckoData = async () => {
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
-  const options = {
+  const options: RequestInit = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      'x-cg-demo-api-key': process.env.COIN_GECKO_TOKEN,
+      'x-cg-demo-api-key': process.env.COIN_GECKO_TOKEN || '',
     },
   };
 
