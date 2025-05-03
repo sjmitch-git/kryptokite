61aa52cdf986536ebd2c1b7ba25401d12b07769a
sjmitch-git <sjmitch20@outlook.com>
2025-05-03 08:38:38 +0100

    Refactors news display

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,3 @@
-import { blueGray } from "tailwindcss/colors";
-
 export const COINGECKO_API_URL = "https://api.coingecko.com/api/v3";
 export const COIN_GECKO_TOKEN = process.env.COIN_GECKO_TOKEN;
 
