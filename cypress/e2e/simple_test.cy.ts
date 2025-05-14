003a15b2da9317a7b04494618f0e089b090bc596
sjmitch-git <sjmitch20@outlook.com>
2025-05-14 18:59:10 +0100

    Adds maintanance mode

--- cypress/e2e/simple_test.cy.ts
+++ cypress/e2e/simple_test.cy.ts
@@ -1,9 +1,10 @@
-describe("Maintenance Page", () => {
-  it("should display the main header", () => {
-    // Visit the home page
-    cy.visit("/maintenance");
+describe("Test Page", () => {
+  it("should display a 404 error if the page is not found", () => {
+    // Visit a non-existent page
+    cy.visit("/non-existent-page", { failOnStatusCode: false });
 
-    // Check if the main header is visible
-    cy.contains("We'll Be Back Soon!").should("be.visible");
+    // Check if the 404 error message is displayed
+    cy.contains("404").should("be.visible");
+    cy.contains("Not Found").should("be.visible");
   });
 });
