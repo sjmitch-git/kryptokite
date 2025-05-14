bdc5de25a97e62e701ac53def75189e36a63e582
sjmitch-git <sjmitch20@outlook.com>
2025-05-14 19:10:30 +0100

    Adds maintanance mode

--- cypress/e2e/simple_test.cy.ts
+++ cypress/e2e/simple_test.cy.ts
@@ -1,10 +1,15 @@
 describe("tEST Page", () => {
   it("should display the maintenance page when the site is under maintenance", () => {
     // Visit a non-existent page (redirects to maintenance.html)
-    cy.visit("/non-existent-page", { failOnStatusCode: false });
+    cy.visit("/", { failOnStatusCode: false });
+
+    cy.url().then((url) => {
+      cy.log("Redirected URL:", url);
+      expect(url).to.include("/maintenance.html");
+    });
 
     // Check if the maintenance page is displayed
-    cy.contains("We'll Be Back Soon!").should("be.visible");
-    cy.contains("Our website is currently undergoing scheduled maintenance.").should("be.visible");
+    //cy.contains("We'll Be Back Soon!").should("be.visible");
+    // cy.contains("Our website is currently undergoing scheduled maintenance.").should("be.visible");
   });
 });
