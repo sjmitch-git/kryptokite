ebe1379eeb56d75b55c18855a95e04d3a56b33ae
sjmitch-git <sjmitch20@outlook.com>
2025-05-14 18:02:05 +0100

    Adds maintanance mode

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: ["cypress/e2e/**/api_routes.cy.js"],
   },
 
   component: {
