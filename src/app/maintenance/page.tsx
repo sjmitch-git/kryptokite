0517ecf145221433d4dff6e6b4c30e5a5836d2b0
sjmitch-git <sjmitch20@outlook.com>
2025-05-15 09:00:40 +0100

    Adds Edge Config

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -2,7 +2,7 @@ const MaintenancePage = () => {
   return (
     <article>
       <img src="./logo.png" alt="Logo" />
-      <h1>We'll Be Back Soon!</h1>
+      <h1>We&apos;ll Be Back Soon!</h1>
       <p>Our website is currently undergoing scheduled maintenance.</p>
       <p>Thank you for your patience. Please check back later.</p>
     </article>
