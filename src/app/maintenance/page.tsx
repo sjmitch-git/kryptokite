ff500b89eb4f816ac6ef917cc5712fc6719eb0a2
sjmitch-git <sjmitch20@outlook.com>
2025-05-15 08:57:32 +0100

    Adds Edge Config

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,12 @@
+const MaintenancePage = () => {
+  return (
+    <article>
+      <img src="./logo.png" alt="Logo" />
+      <h1>We'll Be Back Soon!</h1>
+      <p>Our website is currently undergoing scheduled maintenance.</p>
+      <p>Thank you for your patience. Please check back later.</p>
+    </article>
+  );
+};
+
+export default MaintenancePage;
---
+++ src/app/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all pages and API routes except:
+     * - The maintenance page (`/maintenance`)
+     */
+    "/((?!maintenance).*)",
+  ],
+};
+
+export async function middleware(req: NextRequest) {
+  try {
+    const isInMaintenanceMode = await get<boolean>("maintenance");
+
+    if (isInMaintenanceMode) {
+      const maintenanceUrl = req.nextUrl.clone();
+      maintenanceUrl.pathname = "/maintenance";
+      return NextResponse.rewrite(maintenanceUrl);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error message:", error.message);
+    } else {
+      console.error("Unknown error:", error);
+    }
+  }
+}
