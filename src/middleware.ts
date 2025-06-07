e59feb7b5432af04c05cee19a7f8b4dbe6bcb076
sjmitch-git <sjmitch20@outlook.com>
2025-06-07 21:34:21 +0100

    Middleware update to block IP addresses and certain bots

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,12 @@ export async function middleware(req: NextRequest) {
     const userAgent = req.headers.get("user-agent") || "";
 
     // Block specific IPs and DataForSeoBot
-    if (ip === "98.80.220.72" || ip === "44.221.67.158" || userAgent.includes("DataForSeoBot")) {
+    if (
+      ip === "98.80.220.72" ||
+      ip === "98.80.126.104" ||
+      ip === "44.221.67.158" ||
+      userAgent.includes("DataForSeoBot")
+    ) {
       return new NextResponse("Forbidden: Your IP or user-agent is blocked", { status: 403 });
     }
 
