4a300f1ea8c6a21998e3231e9014179f805d6ea6
sjmitch-git <sjmitch20@outlook.com>
2025-06-22 12:53:45 +0100

    Updates maintenance page

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,7 +1,7 @@
 const MaintenancePage = () => {
   return (
-    <article className="flex flex-col items-center justify-center min-h-50 p-6 text-light space-y-8">
-      <img src={`${process.env.NEXT_PUBLIC_API_URL}logo.png`} alt="Logo" />
+    <article className="flex flex-col items-center justify-center h-full p-6 text-light space-y-8 bg-gradient-to-b from-primary-dark from-20% to-primary-light to-90%">
+      <img src={`${process.env.NEXT_PUBLIC_API_URL}logo.png`} alt="Logo" className='invert' />
       <h1 className="text-3xl font-bold">We&apos;ll Be Back Soon!</h1>
       <p className="text-lg mb-2">Our website is currently undergoing critical maintenance.</p>
       <p className="text-base">Thank you for your patience. Please check back later.</p>
